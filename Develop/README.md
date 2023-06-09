e563777d1bbc8f783601eae1b52cdad16776c278
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-06-09 17:23:32 -0400

    Added writeFile function and test README to develop folder

---
+++ Develop/README.md
@@ -0,0 +1,35 @@
+# NHL Fan Plan
+  Attach license badge here
+  
+  ## Description
+  
+  A simple dashboard to keep track of your favorite hockey stats
+  
+  ## Table of Contents
+  
+  - [Intallation](#installation)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Testing](#testing)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  No installation required
+  
+  ## Usage
+  
+  Simply got to the launches application and interact
+  
+  ## Contributing
+  
+  Please do not contribute
+  
+  ## Testing
+  
+  No testing is necessary
+  
+  ## Questions
+  If you have any additional questions, feel free to contact me on Github @jaxson-lybbert.
+  Or by email: jax.a.lybb@gmail.com
--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generateMarkdown = require("generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -54,12 +54,16 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(`${fileName}.md`, generateMarkdown(data), (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
+}
 
 // TODO: Create a function to initialize app
 function init() {
   inquirer.prompt(questions).then((response) => {
-    console.log(response);
+    writeToFile("README", response);
   });
 }
 
